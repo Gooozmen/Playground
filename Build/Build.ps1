50267b9a444d059d0b2a1c4734aa6c9263a3ee18
Gooozmen <jguzman.softdev@gmail.com>
2024-12-20 23:46:53 -0300

    Build Update - Remove local test variable

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -1,5 +1,4 @@
-.".\Setup.ps1"
-$environment = "local"
+$environment = ""
 if($environment -eq "local"){$psakeFilePath = Resolve-Path ".\psakefile.ps1"}
 else{$psakeFilePath = Resolve-Path ".\Build\psakefile.ps1"}
 & (Resolve-Path "..\Dependencies\psake*\tools\psake\psake.ps1") $psakeFilePath Build
