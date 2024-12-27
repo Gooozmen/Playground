b0ff18d93a3786fa83cf33e1ed089ea01ffc54c9
Gooozmen <jguzman.softdev@gmail.com>
2024-12-27 13:34:59 -0300

    tests added

--- src/Domain/Options/WeatherstackApi.cs
+++ src/Domain/Options/WeatherstackApi.cs
@@ -5,4 +5,12 @@ public class WeatherstackApi
 {
     public string BaseUrl { get; set; }
     public string Key { get; set; }
+    public WeatherstackApi(string baseUrl, string key)
+    {
+        BaseUrl = baseUrl;
+        Key = key;
+    }
+    public WeatherstackApi()
+    {
+    }
 }
